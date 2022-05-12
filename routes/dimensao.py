d4ca0b5b8bd7a261653010b12d1c4cebb03a88e4
Luan Henrique <luan.henrique@venhapranuvem.com>
2022-05-12 00:30:17 -0300

    method get dimension

--- routes/dimensao.py
+++ routes/dimensao.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from controller.dimensao import createDimension, deleteDimension
+from controller.dimensao import createDimension, deleteDimension, getDimension
 
 dimension_bp = Blueprint('dimensao_bp', __name__)
 dimension_bp.route('/createDimension', methods=['POST'])(createDimension)
 dimension_bp.route('/deleteDimension', methods=['DELETE'])(deleteDimension)
+dimension_bp.route('/getDimension', methods=['GET'])(getDimension)
