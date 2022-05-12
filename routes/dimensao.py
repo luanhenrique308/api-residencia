7e0cf65b27672de9d0ec2fb0397cff1ad9d927fd
Luan Henrique <luan.henrique@venhapranuvem.com>
2022-05-12 00:17:25 -0300

    criando method delete dimensao

--- routes/dimensao.py
+++ routes/dimensao.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-from controller.dimensao import createDimension
+from controller.dimensao import createDimension, deleteDimension
 
 dimension_bp = Blueprint('dimensao_bp', __name__)
 dimension_bp.route('/createDimension', methods=['POST'])(createDimension)
+dimension_bp.route('/deleteDimension', methods=['DELETE'])(deleteDimension)
